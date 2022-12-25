7f77d0ada072d580a5af467eedce6f109eedb60f
Guilherme S <guinho.shuto@gmail.com>
2022-12-25 16:19:05 -0400

    ajustezinho de css

--- src/components/Quote.tsx
+++ src/components/Quote.tsx
@@ -1,6 +1,6 @@
 export default function Quote({quote}: any){
     return(
-        <div className="text-left py-4 md:px-8 text-xl md:mx-32 mx-16 bg-bege text-marronzinho hover:text-quase-preto hover:shadow-lg">
+        <div className="text-left py-4 md:px-32 text-xl md:mx-32 mx-16 bg-bege text-marronzinho hover:text-quase-preto hover:shadow-lg">
             #{quote.quoteNumber} - {quote.quote}
         </div>
     )
